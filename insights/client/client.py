6aeab72b7f4fbae5ac6f5c308644f4706442556b
Jeremy Crafts <jcrafts@redhat.com>
2020-02-11 11:36:44 -0500

    fix up archive.py

--- insights/client/client.py
+++ insights/client/client.py
@@ -263,7 +263,7 @@ def collect(config, pconn):
     """
     branch_info = get_branch_info(config)
     pc = InsightsUploadConf(config)
-    tar_file = None
+    output = None
 
     rm_conf = pc.get_rm_conf()
     if rm_conf:
@@ -277,8 +277,8 @@ def collect(config, pconn):
     dc = DataCollector(config, archive, mountpoint=mp)
     logger.info('Starting to collect Insights data for %s', msg_name)
     dc.run_collection(rm_conf, branch_info)
-    tar_file = dc.done(rm_conf)
-    return tar_file
+    output = dc.done(rm_conf)
+    return output
 
 
 def get_connection(config):
